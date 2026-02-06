17a626b6970fe3555da39933169d4887c1475ac6
008kevin <kardoskevin07@gmail.com>
2026-02-06 23:52:15 +0100

    add feature

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const apiBase = "https://surveys-5jvt.onrender.com/api/cars";
-const carContainer = document.getElementById("carsContainer");
+
+// bootstrap
 const carModal = new bootstrap.Modal(document.getElementById('carModal'), {
   keyboard: false
 });
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+tooltipList.forEach((e) => e.disable());
+
+// data container
+const carContainer = document.getElementById("carsContainer");
+
+// info modal fields
 const carInfoId = document.getElementById("carInfoId");
 const carInfoMake = document.getElementById("carInfoMake");
 const carInfoModel = document.getElementById("carInfoModel");
 const carInfoYear = document.getElementById("carInfoYear");
+
+// add car inputs
+const addCarButton = document.getElementById("addCarButton");
+const addBrandInput = document.getElementById("newBrandInput");
+const addBrandTooltip = bootstrap.Tooltip.getInstance('#newBrandInput');
+const addModelInput = document.getElementById("newModelInput");
+const addModelTooltip = bootstrap.Tooltip.getInstance('#newModelInput');
+const addYearInput = document.getElementById("newYearInput");
+const addYearTooltip = bootstrap.Tooltip.getInstance('#newYearInput');
 
 setup();
 
@@ -58,7 +76,55 @@ async function removeCar(id) {
     }
 }
 
+addCarButton.addEventListener("click", async () => {
+    let brand = addBrandInput.value;
+    let isCorrect = true;
+    if (!brand) {
+        isCorrect = false;
+        addBrandInput.classList.add("is-invalid");
+        addBrandTooltip.setContent({ '.tooltip-inner': 'Adjon meg egy márkát!' });
+        addBrandTooltip.enable();
+        addBrandTooltip.show();
+    }
+    let model = addModelInput.value;
+    if (!model) {
+        isCorrect = false;
+        addModelInput.classList.add("is-invalid");
+        addModelTooltip.setContent({ '.tooltip-inner': 'Adjon meg egy modellt!' });
+        addModelTooltip.enable();
+        addModelTooltip.show();
+    }
+    let year = Number(addYearInput.value);
+    if (isNaN(year) || !year) {
+        isCorrect = false;
+        addYearInput.classList.add("is-invalid");
+        addYearTooltip.setContent({ '.tooltip-inner': 'Adjon meg egy évjáratot!' });
+        addYearTooltip.enable();
+        addYearTooltip.show();
+    }
+    if (isCorrect) {
+        try {
+            await post(model, brand, year);
+            setup();
+        } catch (e) {
+            window.alert(e);
+        }
+    }   
+});
 
+function removeInvalidInput(input, tooltip) {
+    input.classList.remove("is-invalid");
+    tooltip.hide();
+    tooltip.disable();
+}
+addBrandInput.addEventListener("input", () => removeInvalidInput(addBrandInput, addBrandTooltip));
+addModelInput.addEventListener("input", () => removeInvalidInput(addModelInput, addModelTooltip));
+addYearInput.addEventListener("input", () => removeInvalidInput(addYearInput, addYearTooltip));
+
+
+/*
+  FETCH METODUSOK
+*/
 async function get() {
     const response = await fetch(apiBase);
     const json = await response.json();
@@ -71,8 +137,8 @@ async function getById(id) {
     return json;
 }
 
-function post(model, brand, year) {
-    return fetch(apiBase, {
+async function post(model, brand, year) {
+    const response = await fetch(apiBase, {
         method: 'POST',
         body: JSON.stringify({
             "model": model,
@@ -80,15 +146,14 @@ function post(model, brand, year) {
             "year": year
         }),
         headers: {
-            'content-type': "application/json; charset=UTF=8"
+            'content-type': "application/json; charset=UTF-8"
         }
-    })
-    .then((request) => {
-        return request.json();
-    })
-    .then((json) => {
-        return json;
     });
+    const json = await response.json();
+        if (!response.ok) {
+        throw `Nem sikerült hozzáadni az adatot! (${json.message})`;
+    }
+    return json;
 }
 
 function put(id, model, brand, year) {
@@ -100,7 +165,7 @@ function put(id, model, brand, year) {
             'year': year
         },
         headers: {
-            'content-type': "application/json; charset=UTF=8"
+            'content-type': "application/json; charset=UTF-8"
         }
     }))
     .then((request) => {
@@ -115,9 +180,9 @@ async function remove(id) {
     const response = await fetch(apiBase+`/${id}`, {
         method: "DELETE"
     });
-    if (!response.ok) {
-        throw "Az adatot nem sikerült törölni!";
-    }
     const json = await response.json();
+    if (!response.ok) {
+        throw `Az adatot nem sikerült törölni! (${json.message})`;
+    }
     return json;
 }
